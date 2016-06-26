90dd28201d0aefba5d0a845ec672572a717cfb64
Vibhor <bijoy.vibhor19@gmail.com>
2016-06-26 11:11:16 +0530

    Update ball.java

--- ball.java
+++ ball.java
@@ -7,14 +7,34 @@ double xVel,yVel,x,y;
 public Ball(){
 	x=300;
 	y=200;
-	xVel=4;
-	yVel=1;
+	xVel=getRandomSpeed()*getRandomDirection();
+		yVel=getRandomSpeed()*getRandomDirection();
 	
 }
-
+public double getRandomSpeed(){
+	return(Math.random() *4+ 2);
+}
+public int getRandomDirection(){
+	int rand=(int)(Math.random() *2);
+	if(rand==1)
+		return 1;
+	else
+		return -1;
+	}
 public void draw(Graphics g){
 	g.setColor(Color.white);
 	g.fillOval((int)x-10,(int) y-10, 20, 20);
+}
+public void checkPaddleCollision(Paddle p1, Paddle p2) {
+	if(x<=50){
+	if(y>=p1.getY()&& y<=p1.getY()+80)	
+		xVel=-xVel;
+	}
+	else if(x>=650){
+		if(y>=p2.getY()&&y<=p2.getY() + 80)
+			xVel=-xVel;
+		
+	}
 }
 public void move(){
 	x+=xVel;
